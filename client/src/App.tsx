9444391bcf7ce3ff4dcbecb6e4d92a28149d66fc
Aritra Banerjee <103491935+AritraLeo@users.noreply.github.com>
2024-05-18 17:27:32 +0530

    Socket conn established btw server and client message log by server

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,33 +1,31 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
+import { Button, Form, InputGroup } from 'react-bootstrap'
+import { useSocket } from './context/SocketProvider'
 
 function App() {
-  const [count, setCount] = useState(0)
+
+  const { sendMessage } = useSocket();
+  const [message, setMessage] = useState('')
 
   return (
     <>
       <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+        <div>
+          <h1>Messages</h1>
+        </div>
+        <div className='d-flex flex-row'>
+          <InputGroup className="mb-3">
+            <Form.Control
+              placeholder="Recipient's username"
+              aria-label="Recipient's username"
+              aria-describedby="basic-addon2"
+              onChange={(e) => setMessage(e.target.value)}
+            />
+            <Button onClick={(e) => sendMessage(message)}>Send</Button>
+          </InputGroup>
+        </div>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
   )
 }
--- client/src/context/SocketProvider.tsx
+++ client/src/context/SocketProvider.tsx
@@ -1,5 +1,5 @@
-import React, { useCallback, useEffect } from "react";
-import { io } from "socket.io-client";
+import React, { useCallback, useContext, useEffect, useState } from "react";
+import { Socket, io } from "socket.io-client";
 
 interface SocketProviderProps {
     children?: React.ReactNode;
@@ -11,18 +11,31 @@ interface ISocketContext {
 
 const SocketContext = React.createContext<ISocketContext | null>(null);
 
+export const useSocket = () => {
+    const state = useContext(SocketContext);
+    if (!state) throw new Error('state is undefined');
+
+    return state;
+}
+
 export const SocketProvider: React.FC<SocketProviderProps> = ({ children }) => {
 
+    const [socket, setSocket] = useState<Socket>()
 
     const sendMessage: ISocketContext['sendMessage'] = useCallback((msg) => {
         console.log('Send Message: ', msg);
-    }, []);
+        if (socket) {
+            socket.emit('event:message', { message: msg });
+        }
+    }, [socket]);
 
     useEffect(() => {
         const _socket = io('http://localhost:8000')
+        setSocket(_socket);
 
         return () => {
             _socket.disconnect();
+            setSocket(undefined);
         }
     }, [])
 
